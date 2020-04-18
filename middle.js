bab44b66ebb88744d47eea936577bb886bce1295
Danny Monroy <danny.p.monroy@gmail.com>
2020-04-18 15:54:08 +0100

    Create all challange urls

--- middle.js
+++ middle.js
@@ -3,25 +3,27 @@ const $ = require('cheerio');
 const fs = require('fs');
 
 const rawdata = fs.readFileSync('urls.json');
-const url = JSON.parse(rawdata);
+const urls = JSON.parse(rawdata);
 
-rp(url[0])
-  .then(function(html){
-    //success!
-    const challangeLinks = [];
-    const selector = 'td > img + a';
-    const leng = $(selector,html).length;
-    for (let i = 0; i < leng; i++) {
-      challangeLinks.push($(selector, html)[i].attribs.href);
-    }
-    return challangeLinks;
-  })
-  .then((data) =>{
-    fs.writeFile('middleurls.json', JSON.stringify(data), (err) =>{
-      if (err) throw err;
-      console.log('The URL file has been created');
+urls.forEach(url => {
+  rp(url)
+    .then(function(html){
+      //success!
+      const challangeLinks = [];
+      const selector = 'td > img + a';
+      const leng = $(selector,html).length;
+      for (let i = 0; i < leng; i++) {
+        challangeLinks.push($(selector, html)[i].attribs.href);
+      }
+      return challangeLinks;
     })
-  })
-  .catch(function(err){
-    //handle error
-  });
+    .then((data) => {
+      fs.appendFile('middleurls.json', JSON.stringify(data), (err) =>{
+        if (err) throw err;
+        console.log(`The challange URLs for ${url} has been created`);
+      })
+    })
+    .catch(function(err){
+      //handle error
+    });
+})
