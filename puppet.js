512964c291f72d6da4ef19b4995168e15849bfaa
Danny Monroy <danny.p.monroy@gmail.com>
2020-04-18 14:26:24 +0100

    Create object with name and description of the challange

--- puppet.js
+++ puppet.js
@@ -1,6 +1,7 @@
 const puppeteer = require('puppeteer');
 const $ = require('cheerio');
 const exampleUrl = 'https://codingbat.com/prob/p187868';
+const obj = {};
 
 puppeteer
 .launch()
@@ -13,8 +14,10 @@ puppeteer
   });
 })
 .then(html => {
-  console.log(html);
+  obj.name = $('span[class = "h2"]', html)[1].children[0].data;
+  obj.description = $('p[class = "max2"]', html)[0].children[0].data;
+  console.dir(obj);
 })
 .catch(err => {
-  throw err;
+  throw err + " code: 2";
 })
