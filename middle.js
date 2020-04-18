354c28188915f8f493b90b0f686220fe7c79bc8c
Danny Monroy <danny.p.monroy@gmail.com>
2020-04-18 16:50:43 +0100

    Moving from array based file to an object based file

--- middle.js
+++ middle.js
@@ -4,24 +4,34 @@ const fs = require('fs');
 
 const rawdata = fs.readFileSync('urls.json');
 const urls = JSON.parse(rawdata);
+console.log({urls});
 
 urls.forEach(url => {
   rp(url)
     .then(function(html){
       //success!
-      const challangeLinks = [];
+      let challangeLinks = [];
+      const links = [];
       const selector = 'td > img + a';
       const leng = $(selector,html).length;
       for (let i = 0; i < leng; i++) {
-        challangeLinks.push($(selector, html)[i].attribs.href);
+        if(i === leng){
+          links.push('"'+$(selector, html)[i].attribs.href+'"');
+        } else{
+          links.push('"'+$(selector, html)[i].attribs.href+'",');
+        }
+        challangeLinks += [...links][i];
       }
       return challangeLinks;
     })
     .then((data) => {
-      fs.appendFile('middleurls.json', JSON.stringify(data), (err) =>{
-        if (err) throw err;
-        console.log(`The challange URLs for ${url} has been created`);
-      })
+      // data = `[${data}]`;
+      // //data = `"${url.substring(26, url.length)}": [${data}],\n`;
+      //   // console.log(data);
+      // fs.appendFile('middleurls.txt', data, (err) =>{
+      //   if (err) throw err;
+      //   console.log(`The challange URLs for ${url} has been created`);
+      // })
     })
     .catch(function(err){
       throw err + " Something went wrong on the middle.js while creating the challange URL's";
