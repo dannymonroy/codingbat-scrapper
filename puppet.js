3dbc93c6e3ee6dfdd64cf314fbae39fe2bbeebd5
Danny Monroy <danny.p.monroy@gmail.com>
2020-04-18 14:55:39 +0100

    Create formatChallange function

--- puppet.js
+++ puppet.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 const $ = require('cheerio');
 const exampleUrl = 'https://codingbat.com/prob/p187868';
-const obj = {};
+const challange = {url: exampleUrl};
 
 puppeteer
 .launch()
@@ -14,10 +14,33 @@ puppeteer
   });
 })
 .then(html => {
-  obj.name = $('span[class = "h2"]', html)[1].children[0].data;
-  obj.description = $('p[class = "max2"]', html)[0].children[0].data;
-  console.dir(obj);
+  challange.name = $('span[class = "h2"]', html)[1].children[0].data;
+  challange.description = $('p[class = "max2"]', html)[0].children[0].data;
+
+  const testCases = [];
+  testCases.push($('br',html)[1].next.data);
+  testCases.push($('br',html)[2].next.data);
+  testCases.push($('br',html)[3].next.data);
+
+  challange.testcases = testCases;
+  console.log(formatChallange(challange))
 })
 .catch(err => {
   throw err + " code: 2";
 })
+
+
+function formatChallange (obj) {
+  return `
+  /*
+  Url: ${obj.url}
+  Title: ${obj.name}
+  Description: ${obj.description}
+
+  Test Cases: 
+  ${obj.testcases[0]}
+  ${obj.testcases[0]}
+  ${obj.testcases[0]}
+  */
+  `
+}
