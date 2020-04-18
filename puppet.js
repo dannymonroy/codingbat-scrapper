cd96cc2bb8225c1858a10ed875228f3055f7d568
Danny Monroy <danny.p.monroy@gmail.com>
2020-04-18 13:20:21 +0100

    Get full HTML document using puppeteer

--- puppet.js
+++ puppet.js
@@ -1,8 +1,20 @@
 const puppeteer = require('puppeteer');
-const exampleUrl = 'https://codingbat.com/java/Warmup-1';
+const $ = require('cheerio');
+const exampleUrl = 'https://codingbat.com/prob/p187868';
 
 puppeteer
 .launch()
 .then((browser)=>{
   return browser.newPage();
+})
+.then((page) => {
+  return page.goto(exampleUrl).then(() => {
+    return page.content();
+  });
+})
+.then(html => {
+  console.log(html);
+})
+.catch(err => {
+  throw err;
 })
