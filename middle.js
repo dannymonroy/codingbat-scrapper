3c545167540b24e6aa13e24e550333371a1d838a
Danny Monroy <danny.p.monroy@gmail.com>
2020-04-19 13:03:51 +0100

    Create link object dynamically

--- middle.js
+++ middle.js
@@ -4,36 +4,26 @@ const fs = require('fs');
 
 const rawdata = fs.readFileSync('urls.json');
 const urls = JSON.parse(rawdata);
-const obj = {
-  'http://codingbat.com/java/Warmup-1':[],
-  'http://codingbat.com/java/Warmup-2':[],
-  'http://codingbat.com/java/String-1':[],
-  'http://codingbat.com/java/Array-1':[],
-  'http://codingbat.com/java/Logic-1':[],
-  'http://codingbat.com/java/Logic-2':[],
-  'http://codingbat.com/java/String-2':[],
-  'http://codingbat.com/java/String-3':[],
-  'http://codingbat.com/java/Array-2':[],
-  'http://codingbat.com/java/Array-3':[],
-  'http://codingbat.com/java/AP-1':[],
-  'http://codingbat.com/java/Recursion-1':[],
-  'http://codingbat.com/java/Recursion-2':[],
-  'http://codingbat.com/java/Map-1':[],
-  'http://codingbat.com/java/Map-2':[],
-  'http://codingbat.com/java/Functional-1':[],
-  'http://codingbat.com/java/Functional-2':[],
+
+const createLinkObj = arr => {
+  const obj = {}
+  for(const elem of arr){
+    obj[elem] = [];
+  }
+  return obj
 }
+
+const problemsObj = createLinkObj(urls);
 
 for(const url of urls){
   rp(url)
     .then((html) => {
-      //success!
       const selector = 'td > img + a';
       const leng = $(selector,html).length;
       for (let i = 0; i < leng; i++) {
-        obj[url].push($(selector, html)[i].attribs.href);
+        problemsObj[url].push($(selector, html)[i].attribs.href);
       }
-      return obj;
+      return problemsObj;
       
     }).then((data) => {
       if(url === 'http://codingbat.com/java/Functional-2'){
